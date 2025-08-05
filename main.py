4498a8a7b22b8200a2324cf00a8b96641832423a
SnafuAl <snafual@gmail.com>
2025-08-05 11:55:16 +0100

    Implemented game loop and exit

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from asteroids!")
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
 
+from constants import *
+
+def main():
+
+    # Initialise PyGame
+    pygame.init()
+
+    # Set up screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    while True:
+        # allow exit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
