b564f2697fbbe6d786367784298f5fe95881ffe5
SnafuAl <snafual@gmail.com>
2025-08-06 22:17:42 +0100

    Added shot rate limit

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0.0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -39,8 +40,12 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(0 - dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_timer <= 0:
+                self.shoot()
+
+        self.shot_timer -= dt
 
     def shoot(self):
         new_shot = Shot(self.position.x, self.position.y)
         new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
